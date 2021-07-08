daf0f4f6caa25142062e22e21db5cfd56dfec509
ElifToraman <79575300+ElifToraman@users.noreply.github.com>
2021-07-08 12:15:57 +0300

    Update App.js

--- autocomplete/src/App.js
+++ autocomplete/src/App.js
@@ -1,20 +1,20 @@
-import React,{Component} from 'react';
+import React from 'react';
 import './App.css';
-import Text from './Text';
-import Data from './countries.json';
-//import countries from './countries';
+import Text from './components/Text';
+import countryNames from './data/countries.json';
+
 
 function App() {
-  var countriesToSelect = Data.map(item => item.name + '-' + item.code);
+  
+  var countriesToSelect = [];
+  if(countryNames && countryNames.length>0) countriesToSelect=countryNames.map(item => item.name + '-' + item.code);
 
   return (
     <div className="App">
-      <div className="App-Component">
-         <h2 style={{fontFamily:"Times New Roman",fontSize:"40px"}}>Select Country</h2>
-           <div className="App-Component">
-             <Text items={countriesToSelect} />
-          </div>
-      </div>
+      <h2 className="App-Container">Select Country</h2>
+        <div className="App-Component">
+          <Text items={countriesToSelect} />
+        </div>
     </div>
   );
 }
