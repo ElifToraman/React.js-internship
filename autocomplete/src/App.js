9a45cff3bde135b49b8ed8b149060f1e21da6000
okandavutosf <okan.davut@osf.digital>
2021-07-09 15:57:46 +0300

    Code review solutions

--- autocomplete/src/App.js
+++ autocomplete/src/App.js
@@ -6,15 +6,17 @@ import HookTest from './components/HookTest';
 
 
 function App() {
-  
   var countriesToSelect = [];
-  if(countryNames && countryNames.length>0) countriesToSelect=countryNames.map(item => item.name + '-' + item.code);
+
+  if(countryNames && countryNames.length>0) {
+     countriesToSelect=countryNames.map(item => item.name + '-' + item.code);
+  }
 
   return (
     <div className="App">
       <h2 className="App-Container">Select Country</h2>
         <div className="App-Component">
-          <Text items={countriesToSelect} />
+          <HookTest items={countriesToSelect} />
         </div>
     </div>
   );
